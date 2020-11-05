077fb0ad06148fbf2a734863635d766dd1f1335a
Barney Nicholls <barney.nicholls@bluetoad.net>
2020-11-05 23:02:12 +0000

    test steps for enemy ai and fps control

---
+++ Assets/Headtalks/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController controller;
+
+    public float speed = 12f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
