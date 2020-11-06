bef01e27bb89d6ff3eb4af8f255e9a419f7af7df
barneynicholls <barney.nicholls@bluetoad.net>
2020-11-06 18:21:38 +0000

    added weapon

---
+++ Assets/Headtalks/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Transform endOfBarrel;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, endOfBarrel.position, endOfBarrel.rotation);
+
+        var rb = bullet.GetComponent<Rigidbody>();
+
+        rb.AddForce(camera.forward * 2000f);
+    }
+}
